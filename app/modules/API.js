aa053f2d55bc06f08018149928e5796f529929fe
Nadrieril <nadrieril@gmail.com>
2014-06-10 04:09:32 +0200

    First Working API interaction

---
+++ app/modules/API.js
@@ -0,0 +1,33 @@
+'use strict';
+
+angular.module('bars.API', [
+	'ngResource'
+])
+
+.factory('API', [
+	function () {
+		var barId="bar";
+		return {
+			setBarId: function(barid){
+				barId=barid
+			},
+			route: function(path){
+				// return '/'+barId+'/'+path;
+				return '/../../bars-symfony/web/'+barId+'/'+path;
+			}
+		};
+}])
+
+.factory('API.User', ['$resource', 'API',
+  function($resource, API){
+	return $resource(API.route('user/:id'), {}, {
+	  query: {method:'GET', isArray:true}
+	});
+  }])
+.factory('API.Food', ['$resource', 'API',
+  function($resource, API){
+	return $resource(API.route('food/:id'), {}, {
+	  query: {method:'GET', isArray:true},
+	  search: {method:'GET', url:API.route('food/search/:q'), isArray:true}
+	});
+  }]);
