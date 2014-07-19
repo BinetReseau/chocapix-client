ca33e081fe041b8119521ac44e71219cd587abbb
tizot <masset.camille@gmail.com>
2014-07-19 17:09:03 +0200

    Makes code clearer

--- app/scripts/controllers/history.js
+++ app/scripts/controllers/history.js
@@ -3,7 +3,10 @@
 angular.module('bars.ctrl.history', [
 	])
 	.controller('HistoryCtrl',
-		['$scope', 'API.Transaction', 'history', function($scope, Transaction, history) {
+		['$scope', 
+		'API.Transaction', 
+		'history', 
+		function($scope, Transaction, history) {
 			$scope.bar.active = 'history';
 			$scope.history = history;
 			$scope.updateHistory = function() {
@@ -12,5 +15,6 @@ angular.module('bars.ctrl.history', [
 				    $scope.updatingHistory = false;
 			    });
 			};
-		}])
+		}
+	])
 ;
