9b2669b2ec4a44dfd42f97c94615c23906c66224
Basile Bruneau <basilebruneau@gmail.com>
2014-07-21 00:21:25 +0200

    Add throw on food page

--- app/scripts/controllers/food.js
+++ app/scripts/controllers/food.js
@@ -6,17 +6,28 @@ angular.module('bars.ctrl.food', [])
 		['$scope', '$stateParams', 'API.Food',
 		function($scope, $stateParams, Food) {
 			$scope.FoodDetails = Food.get({id: $stateParams.id});
-			$scope.buyQty = 1;
-			$scope.buy = function(qty) {
-				var Transaction = Food.buy({item: $stateParams.id, qty: qty}, function () {
-					for (var  i = 0 ; i < Transaction.operations.length ; i++) {
-						if (Transaction.operations[i].type == 'stockoperation' && Transaction.operations[i].item.id == $stateParams.id) {
-							$scope.FoodDetails = Transaction.operations[i].item;
-						} else if (Transaction.operations[i].type == 'accountoperation') {
-							$scope.user.account.money = Transaction.operations[i].account.money;
+			$scope.queryQty = 1;
+			$scope.queryType = 'buy';
+			$scope.query = function(qty, type) {
+				if (type == 'buy') {
+					var Transaction = Food.buy({item: $stateParams.id, qty: qty}, function () {
+						for (var  i = 0 ; i < Transaction.operations.length ; i++) {
+							if (Transaction.operations[i].type == 'stockoperation' && Transaction.operations[i].item.id == $stateParams.id) {
+								$scope.FoodDetails = Transaction.operations[i].item;
+							} else if (Transaction.operations[i].type == 'accountoperation') {
+								$scope.user.account.money = Transaction.operations[i].account.money;
+							}
 						}
-					}
-				});
+					});
+				} else if (type == 'throw') {
+					var Transaction = Food.throwaway({item: $stateParams.id, qty: qty}, function () {
+						for (var  i = 0 ; i < Transaction.operations.length ; i++) {
+							if (Transaction.operations[i].type == 'stockoperation' && Transaction.operations[i].item.id == $stateParams.id) {
+								$scope.FoodDetails = Transaction.operations[i].item;
+							}
+						}
+					});
+				}
 			};
 		}])
 ;
