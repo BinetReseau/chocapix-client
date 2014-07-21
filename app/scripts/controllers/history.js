ff7c98c3331ab6d18398db0933086f63362118be
Basile Bruneau <basilebruneau@gmail.com>
2014-07-21 19:14:05 +0200

    Display changes

--- app/scripts/controllers/history.js
+++ app/scripts/controllers/history.js
@@ -9,20 +9,12 @@ angular.module('bars.ctrl.history', [
 		function($scope, Transaction, history) {
 			$scope.bar.active = 'history';
 			$scope.history = history;
+			$scope.canUpdate = true;
 			$scope.updateHistory = function() {
 			    $scope.updatingHistory = true;
 			    $scope.history =  Transaction.query({}, function () {
 				    $scope.updatingHistory = false;
 			    });
-			};
-			$scope.prixTotal = function(h) { // coût total d'une transaction (négatif si on dépense, positif si on est remboursé)
-				var somme = 0;
-				for (var i = 0; i < h.operations.length; i++) {
-					if (h.operations[i].type == 'stockoperation') {
-						somme = somme + (h.operations[i].deltaqty * h.operations[i].item.price);
-					}
-				}
-				return somme;
 			};
 		}
 	])
