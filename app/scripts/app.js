59ae5d244f75fc5a13565d47e4e509c6a1edbc2e
Nadrieril <nadrieril@gmail.com>
2014-07-22 21:56:54 +0200

    Added some directives

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -136,7 +136,7 @@ barsApp.config(['$stateProvider', '$urlRouterProvider',
 				},
 				controller: 'AccountDetailCtrl'
 			})
-			
+
 			.state('bar.history', {
 				url: "/history",
 				templateUrl: "views/history.html",
@@ -159,7 +159,46 @@ barsApp.config(['$httpProvider',
 		$httpProvider.interceptors.push('AuthInterceptor');
 }]);
 
+barsApp.directive('barsFood', function() {
+    return {
+        restrict: 'E',
+        scope: {
+            food: '=food',
+            unit: '=unit',
+            qty: '=qty'
+        },
+        template: function(elt, attrs) {
+            if(attrs.unit === undefined || attrs.qty === undefined) {
+                return '<a title="Voir la fiche de cet aliment" ui-sref="bar.food.detail({ bar: food.bar, id:food.id })">' +
+                        '{{ food.name }}' +
+                    '</a>';
+            } else {
+                return '{{ qty | number }} ' +
+                        '<span ng-if="unit != \'\'">{{ unit }} {{ food.name | affd }}</span>' +
+                        '<a title="Voir la fiche de cet aliment" ui-sref="bar.food.detail({ bar: food.bar, id:food.id })">' +
+                            '<span ng-if="unit != \'\'">{{ food.name }}</span>' +
+                            '<span ng-if="unit == \'\'">{{ food.name | affs:qty }}</span>' +
+                        '</a>';
+            }
+        }
+    };
+});
+barsApp.directive('barsAccount', function() {
+    return {
+        restrict: 'E',
+        scope: {
+            account: '=account'
+        },
+        template: '<a title="En savoir plus sur {{ account.user.name }}" ui-sref="bar.account.detail({ bar: account.bar, id:account.id })">' +
+                    '{{ account.user.name }}' +
+                '</a>'
+    };
+});
+
+
+
+
 barsApp.run(function(amMoment) {
-    amMoment.changeLanguage('fr');
+	amMoment.changeLanguage('fr');
 });
 
