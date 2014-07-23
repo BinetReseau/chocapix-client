7da86557f2d9ad283344d682568990e026a361fd
Nadrieril <nadrieril@gmail.com>
2014-07-24 00:13:35 +0200

    Moved API to APIObject

--- app/scripts/services/API.js
+++ app/scripts/services/API.js
@@ -19,13 +19,13 @@ angular.module('bars.API', [
 		};
 }])
 
-.factory('API.Account', ['$resource', 'API',
-	function($resource, API) {
-		return $resource(API.route('account/:id'), {}, {
-			query: {method:'GET', isArray:true},
-			byUser: {method: 'GET', url:API.route('account/by-user/:id'), isArray:true}
-		});
-	}])
+// .factory('API.Account', ['$resource', 'API',
+// 	function($resource, API) {
+// 		return $resource(API.route('account/:id'), {}, {
+// 			query: {method:'GET', isArray:true},
+// 			byUser: {method: 'GET', url:API.route('account/by-user/:id'), isArray:true}
+// 		});
+// 	}])
 .factory('API.Action', ['$resource', 'API',
 	function($resource, API) {
 		return $resource('', {}, {
@@ -34,23 +34,23 @@ angular.module('bars.API', [
 			throwaway: {method: 'POST', url:API.route('action/throw')}
 		});
 	}])
-.factory('API.Bar', ['$resource', 'API',
-	function($resource, API) {
-		return $resource(API.route(''));
-	}])
-.factory('API.Food', ['$resource', 'API',
-	function($resource, API) {
-		return $resource(API.route('food/:id'), {}, {
-			query: {method:'GET', isArray:true},
-			search: {method:'GET', url:API.route('food/search/:q'), isArray:true}
-		});
-	}])
-.factory('API.Me', ['$resource', 'API',
-	function($resource, API) {
-		return $resource(API.route('account/me'), {}, {
-			all: {method:'GET', url:API.route('../nobar/auth/me')}
-		});
-	}])
+// .factory('API.Bar', ['$resource', 'API',
+// 	function($resource, API) {
+// 		return $resource(API.route(''));
+// 	}])
+// .factory('API.Food', ['$resource', 'API',
+// 	function($resource, API) {
+// 		return $resource(API.route('food/:id'), {}, {
+// 			query: {method:'GET', isArray:true},
+// 			search: {method:'GET', url:API.route('food/search/:q'), isArray:true}
+// 		});
+// 	}])
+// .factory('API.Me', ['$resource', 'API',
+// 	function($resource, API) {
+// 		return $resource(API.route('account/me'), {}, {
+// 			all: {method:'GET', url:API.route('../nobar/auth/me')}
+// 		});
+// 	}])
 // .factory('API.Transaction', ['$resource', 'API',
 // 	function($resource, API) {
 // 		return $resource(API.route('transaction/:id'), {}, {
@@ -67,21 +67,40 @@ angular.module('bars.API', [
 // 		});
 // 	}])
 
+.factory('API.Account', ['APIObject', 'API',
+	function(APIObject, API) {
+		return APIObject(API.route('account/:id'), {}, {
+			byUser: {method: 'GET', url:API.route('account/by-user/:id'), isArray:true, static:true}
+		});
+	}])
+.factory('API.Bar', ['APIObject', 'API',
+	function(APIObject, API) {
+		return APIObject(API.route(''));
+	}])
+.factory('API.Food', ['APIObject', 'API',
+	function(APIObject, API) {
+		return APIObject(API.route('food/:id'), {}, {
+		});
+	}])
+.factory('API.Me', ['APIObject', 'API',
+	function(APIObject, API) {
+		return APIObject(API.route('account/me'), {}, {
+			all: {method:'GET', url:API.route('../nobar/auth/me'), static:true}
+		});
+	}])
 .factory('API.User', ['APIObject', 'API',
 	function(APIObject, API) {
-		return APIObject(API.route('user/:id'), {
-			query: {method:'GET', isArray:true}
-		}, {});
+		return APIObject(API.route('user/:id'), {id:'@id'}, {
+		});
 	}])
 .factory('API.Transaction', ['APIObject', 'API',
 	function(APIObject, API) {
 		return APIObject(
-			API.route('transaction/:id'),
+			API.route('transaction/:id'), {id:'@id'},
 			{
 				cancel: {method:'DELETE'},
-				query: {method:'GET', isArray:true},
-				byAccount: {method:'GET', url:API.route('transaction/by-account/:id'), isArray:true},
-				byItem: {method:'GET', url:API.route('transaction/by-item/:id'), isArray:true}
+				byAccount: {method:'GET', url:API.route('transaction/by-account/:id'), isArray:true, static:true},
+				byItem: {method:'GET', url:API.route('transaction/by-item/:id'), isArray:true, static:true}
 			},
 			{
 				author: {object: 'API.User'},
