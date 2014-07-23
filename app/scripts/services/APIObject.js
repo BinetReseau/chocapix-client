261b425f7b0bc891d2223f8e0dc90a6e52126b0b
Nadrieril <nadrieril@gmail.com>
2014-07-24 01:06:11 +0200

    Added array filter

--- app/scripts/services/APIObject.js
+++ app/scripts/services/APIObject.js
@@ -2,152 +2,150 @@ var module = angular.module('APIObject', [
 	'ngResource'
 ]);
 
-function maxIntKey(obj) {
-	if(obj instanceof Array) return obj.length-1;
-    var max = 0, key;
-    for (key in obj) {
-        if (obj.hasOwnProperty(key) && !!parseInt(key)) {
-        	max = key>max ? key : max;
-        }
-    }
-    return max;
-};
+// // Overrides default angularjs filterFilter
+// module.filter('filter', function() {
+// 	return function(array, expression, comparator) {
+// 		// if (!angular.isArray(array)) return array;
+// 		if(!array || typeof array != "object") return array;
+// 		var comparatorType = typeof(comparator),
+// 			predicates = [];
+// 		predicates.check = function(value) {
+// 			for (var j = 0; j < predicates.length; j++) {
+// 				if (!predicates[j](value)) {
+// 					return false;
+// 				}
+// 			}
+// 			return true;
+// 		};
+// 		if (comparatorType !== 'function') {
+// 			if (comparatorType === 'boolean' && comparator) {
+// 				comparator = function(obj, text) {
+// 					return angular.equals(obj, text);
+// 				};
+// 			} else {
+// 				comparator = function(obj, text) {
+// 					if (obj && text && typeof obj === 'object' && typeof text === 'object') {
+// 						for (var objKey in obj) {
+// 							if (objKey.charAt(0) !== '$' && hasOwnProperty.call(obj, objKey) && comparator(obj[objKey], text[objKey])) {
+// 								return true;
+// 							}
+// 						}
+// 						return false;
+// 					}
+// 					text = ('' + text).toLowerCase();
+// 					return ('' + obj).toLowerCase().indexOf(text) > -1;
+// 				};
+// 			}
+// 		}
+// 		var search = function(obj, text) {
+// 			if (typeof text == 'string' && text.charAt(0) === '!') {
+// 				return !search(obj, text.substr(1));
+// 			}
+// 			switch (typeof obj) {
+// 				case "boolean":
+// 				case "number":
+// 				case "string":
+// 					return comparator(obj, text);
+// 				case "object":
+// 					switch (typeof text) {
+// 						case "object":
+// 							return comparator(obj, text);
+// 						default:
+// 							for (var objKey in obj) {
+// 								if (objKey.charAt(0) !== '$' && search(obj[objKey], text)) {
+// 									return true;
+// 								}
+// 							}
+// 							break;
+// 					}
+// 					return false;
+// 				case "array":
+// 					for (var i = 0; i < obj.length; i++) {
+// 						if (search(obj[i], text)) {
+// 							return true;
+// 						}
+// 					}
+// 					return false;
+// 				default:
+// 					return false;
+// 			}
+// 		};
+// 		switch (typeof expression) {
+// 			case "boolean":
+// 			case "number":
+// 			case "string":
+// 				// Set up expression object and fall through
+// 				expression = {
+// 					$: expression
+// 				};
+// 				// jshint -W086
+// 			case "object":
+// 				// jshint +W086
+// 				for (var key in expression) {
+// 					(function(path) {
+// 						if (typeof expression[path] === 'undefined') return;
+// 						predicates.push(function(value) {
+// 							return search(path == '$' ? value : (value && value[path]), expression[path]);
+// 						});
+// 					})(key);
+// 				}
+// 				break;
+// 			case 'function':
+// 				predicates.push(expression);
+// 				break;
+// 			default:
+// 				return array;
+// 		}
 
-module.filter('filter', function() {
-    return function(array, expression, comparator) {
-        // if (!angular.isArray(array)) return array;
-        if(!array || typeof array != "object") return array;
-        var comparatorType = typeof(comparator),
-            predicates = [];
-        predicates.check = function(value) {
-            for (var j = 0; j < predicates.length; j++) {
-                if (!predicates[j](value)) {
-                    return false;
-                }
-            }
-            return true;
-        };
-        if (comparatorType !== 'function') {
-            if (comparatorType === 'boolean' && comparator) {
-                comparator = function(obj, text) {
-                    return angular.equals(obj, text);
-                };
-            } else {
-                comparator = function(obj, text) {
-                    if (obj && text && typeof obj === 'object' && typeof text === 'object') {
-                        for (var objKey in obj) {
-                            if (objKey.charAt(0) !== '$' && hasOwnProperty.call(obj, objKey) && comparator(obj[objKey], text[objKey])) {
-                                return true;
-                            }
-                        }
-                        return false;
-                    }
-                    text = ('' + text).toLowerCase();
-                    return ('' + obj).toLowerCase().indexOf(text) > -1;
-                };
-            }
-        }
-        var search = function(obj, text) {
-            if (typeof text == 'string' && text.charAt(0) === '!') {
-                return !search(obj, text.substr(1));
-            }
-            switch (typeof obj) {
-                case "boolean":
-                case "number":
-                case "string":
-                    return comparator(obj, text);
-                case "object":
-                    switch (typeof text) {
-                        case "object":
-                            return comparator(obj, text);
-                        default:
-                            for (var objKey in obj) {
-                                if (objKey.charAt(0) !== '$' && search(obj[objKey], text)) {
-                                    return true;
-                                }
-                            }
-                            break;
-                    }
-                    return false;
-                case "array":
-                    for (var i = 0; i < obj.length; i++) {
-                        if (search(obj[i], text)) {
-                            return true;
-                        }
-                    }
-                    return false;
-                default:
-                    return false;
-            }
-        };
-        switch (typeof expression) {
-            case "boolean":
-            case "number":
-            case "string":
-                // Set up expression object and fall through
-                expression = {
-                    $: expression
-                };
-                // jshint -W086
-            case "object":
-                // jshint +W086
-                for (var key in expression) {
-                    (function(path) {
-                        if (typeof expression[path] === 'undefined') return;
-                        predicates.push(function(value) {
-                            return search(path == '$' ? value : (value && value[path]), expression[path]);
-                        });
-                    })(key);
-                }
-                break;
-            case 'function':
-                predicates.push(expression);
-                break;
-            default:
-                return array;
-        }
-        var filtered = [];
-        if(array instanceof Array) {
-	        for (var j = 0; j < array.length; j++) {
-	            var value = array[j];
-	            if (predicates.check(value)) {
-	                filtered.push(value);
-	            }
-	        }
-	    } else {
-	        for (var j = 0; j <= maxIntKey(array); j++) {
-	            var value = array[j];
-	            if (predicates.check(value)) {
-	                filtered.push(value);
-	            }
-	        }
-	    }
-        return filtered;
-    };
+// 		function length(obj) {
+// 			if(obj instanceof Array) return obj.length;
+// 			var max = 0;
+// 			for (var key in obj) {
+// 				if (obj.hasOwnProperty(key) && !!parseInt(key)) {
+// 					max = key>max ? key : max;
+// 				}
+// 			}
+// 			return max + 1;
+// 		};
+
+// 		var filtered = [];
+// 		for (var j = 0; j < length(array); j++) {
+// 			var value = array[j];
+// 			if (predicates.check(value)) {
+// 				filtered.push(value);
+// 			}
+// 		}
+// 		return filtered;
+// 	};
+// });
+module.filter('array', function() {
+	return function(obj) {
+		var length = 0;
+		for (var key in obj) {
+			if (obj.hasOwnProperty(key) && !!parseInt(key)) {
+				length = key>length ? key : length;
+			}
+		}
+
+		var filtered = [];
+		for (var j = 0; j < length; j++) {
+			filtered.push(obj[j]);
+		}
+		return filtered;
+	};
 });
+
 
 (function(module){
 function shallowClearAndCopy(src, dst) {
 	dst = dst || {};
-	angular.forEach(dst, function(value, key) {
-		if(dst.hasOwnProperty(key))
+	for (var key in dst) {
+		if (dst.hasOwnProperty(key)) {
 			delete dst[key];
-	});
-	for (var key in src) {
-		if (src.hasOwnProperty(key) && !(key.charAt(0) === '$' && key.charAt(1) === '$')) {
-			dst[key] = src[key];
 		}
 	}
-	return dst;
-}
-
-function unsafeShallowClearAndCopy(src, dst) {
-	dst = dst || {};
-	angular.forEach(dst, function(value, key) {
-		delete dst[key];
-	});
 	for (var key in src) {
-		if (!(key.charAt(0) === '$' && key.charAt(1) === '$')) {
+		if (src.hasOwnProperty(key) && !(key.charAt(0) === '$' && key.charAt(1) === '$')) {
 			dst[key] = src[key];
 		}
 	}
@@ -166,9 +164,12 @@ function shallowCopy(src, dst) {
 
 function shallowCopyFilter(obj, filter) {
 	var obj2 = {};
+	var filtered;
 	angular.forEach(obj, function(value, key){
-		if(filter(value))
-			obj2[key] = filter(value);
+		filtered = filter(value);
+		if(filtered != undefined) {
+			obj2[key] = filtered;
+		}
 	})
 	return obj2;
 }
@@ -186,13 +187,13 @@ module.factory('APIObject', ['$injector', '$resource', 'API',
 		return function(url, defaults, methods, structure) {
 			structure = structure || {};
 			methods = angular.extend(DEFAULT_METHODS, methods);
-			// var resource = $resource(url, defaults,
-			// 	shallowCopyFilter(methods, function(x){
-			// 		x = angular.copy(x);
-			// 		delete x['object'];
-			// 		delete x['static'];
-			// 	}));
-			var resource = $resource(url, defaults, methods);
+			var resource = $resource(url, defaults,
+				shallowCopyFilter(methods, function(x){
+					x = angular.copy(x);
+					delete x['object'];
+					delete x['static'];
+					return x;
+				}));
 
 			function APIObject(value){
 				shallowCopy(value || {}, this);
@@ -224,28 +225,28 @@ module.factory('APIObject', ['$injector', '$resource', 'API',
 				};
 				function f(is_static){
 					return function(params, data) {
-						console.log(key + '(' + url + ')');
+						console.log(key + '(' + (method.url || url) + ')');
 						var value = {};
 						value.$resolved = false;
 						value.$promise = resource[key](params, is_static ? data : (data || this)).$promise
 								.then(parse)
 								.then(function(o){
 									o.$reload = function(){
-										var o = APIObject[key](params, data);
-										o.$promise.then(function(o){
-											unsafeShallowClearAndCopy(o, value);
-											return value;
+										var o = (is_static ? APIObject : o)[key](params, data);
+										o.$promise.then(function(oo){
+											shallowClearAndCopy(oo, value);
+											return oo;
 										})
 										return o;
 									};
 									return o;
 								})
 								.then(function(o){
-									console.log('returned ' + key + '(' + url + ')');
+									console.log('returned ' + key + '(' + (method.url || url) + ')');
 									console.log(o);
-									unsafeShallowClearAndCopy(o, value);
+									shallowClearAndCopy(o, value);
 									value.$resolved = true;
-									return o;
+									return value;
 								});
 						return value;
 					}
@@ -253,14 +254,22 @@ module.factory('APIObject', ['$injector', '$resource', 'API',
 				if(method.static){
 					APIObject[key] = f(true);
 				} else {
-					APIObject.prototype[key] = f(false);
+					Object.defineProperty(APIObject.prototype, key, {
+						enumerable: false,
+						configurable: true,
+						writable: true,
+						value: f(false)
+					})
+					// APIObject.prototype[key] = f(false);
 				}
 			});
 			APIObject.prototype.$reload = function(){
-				return APIObject.get({id: this.id}).then(function(obj){
-					unsafeShallowClearAndCopy(obj, this);
-					return this;
-				})
+				var o = APIObject.get({id: this.id});
+				o.$promise.then(function(obj){
+					shallowClearAndCopy(obj, this);
+					return obj;
+				});
+				return o;
 			};
 			APIObject.prototype.$reloadAll = function(){
 				return this.$parent ? this.$parent.$reload(): this.$reload();
