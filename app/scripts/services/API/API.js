e8f4d6b50b5a693bcd73daefaf3c1746fe5c5b4a
tizot <masset.camille@gmail.com>
2014-07-29 12:49:40 +0200

    Adds delete and undelete StockItem route

--- app/scripts/services/API/API.js
+++ app/scripts/services/API/API.js
@@ -41,7 +41,9 @@ angular.module('bars.API', [
 .factory('API.Food', ['APIObject', 'API',
 	function(APIObject, API) {
 		return APIObject(API.route('food/:id'), {}, {
-			add: {method: 'POST', url:API.route('food/add'), static: true, object: 'API.Transaction'}
+			add: {method: 'POST', url:API.route('food/add'), static: true, object: 'API.Transaction'},
+			remove: {method: 'DELETE', url:API.route('food/:id')},
+			unremove: {method: 'POST', url:API.route('food/undelete/:id')}
 		});
 	}])
 .factory('API.Me', ['APIObject', 'API',
