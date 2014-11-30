2dcd3c07a278af3e295ef5b8d397ccc9596bb4c5
Nadrieril <nadrieril@gmail.com>
2014-11-30 11:45:33 +0100

    Added redirection from /:bar/user/:id to corresponding /:bar/account/:id

--- app/components/API/user/user.js
+++ app/components/API/user/user.js
@@ -14,4 +14,30 @@ angular.module('bars.api.user', [
                 }
             });
     }])
+
+.config(['$stateProvider', function($stateProvider) {
+    $stateProvider
+        .state('bar.user', {
+            url: "/user",
+            abstract: true,
+            template:'<ui-view/>'
+        })
+        .state('bar.user.detail', {
+            url: "/:id",
+            resolve:{
+                account: ['APIInterface', 'api.models.account', '$stateParams',
+                    function(APIInterface, Account, $stateParams) {
+                        return APIInterface.request({
+                            method: "GET",
+                            url:"account/?bar="+$stateParams.bar+"&owner="+$stateParams.id
+                        }).then(function(o) {
+                            return o[0];
+                        });
+                }]
+            },
+            controller: ['$state', 'account', function($state, account){
+                $state.go('bar.account.details', {'id':account.id});
+                }]
+        });
+}])
 ;
