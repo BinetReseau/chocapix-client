4e3d45f02a195a15fb4f9447f1cf2177318bef68
Basile Bruneau <basilebruneau@gmail.com>
2014-12-22 09:42:33 +0100

    Meal service is used

--- app/components/meal/meal.js
+++ app/components/meal/meal.js
@@ -9,78 +9,6 @@ angular.module('bars.meal', [
         Meal.account = $scope.user.account;
         Meal.init();
         $scope.meal = Meal;
-        function init() {
-            $scope.customersList = [ { account: $scope.user.account, ratio: 1, amount: 0 } ];
-            $scope.itemsList = [];
-            $scope.totalPrice = 0;
-            $scope.accountToAdd = "";
-        }
-        init();
-
-        $scope.recomputeAmount = function() {
-            var nbParts = 0; // nombre de parts pour le calcul (somme des ratios)
-            _.forEach($scope.customersList, function(customer) {
-                nbParts += customer.ratio;
-            });
-
-            var nbCustomers = $scope.customersList.length;
-            var nbItems = $scope.itemsList.length;
-
-            var totalPrice = 0;
-            _.forEach($scope.itemsList, function(item, i) {
-                totalPrice += item.item.price * item.buy_qty * item.item.unit_value;
-            });
-
-            _.forEach($scope.customersList, function(customer) {
-                customer.amount = totalPrice * customer.ratio / nbParts;
-            });
-            $scope.totalPrice = totalPrice;
-        };
-
-        $scope.addCustomer = function(account, model, label) {
-            var other = _.find($scope.customersList, {'account': account});
-            if (!other) {
-                $scope.customersList.push({ account: account, ratio: 1, amount: 0 });
-            }
-            $scope.accountToAdd = '';
-            $scope.recomputeAmount();
-        };
-        $scope.removeCustomer = function(cstmr) {
-            $scope.customersList.splice($scope.customersList.indexOf(cstmr), 1);
-            $scope.recomputeAmount();
-        };
-
-        $scope.addItem = function(item, model, label) {
-            var other = _.find($scope.itemsList, {'item': item});
-            if (other) {
-                other.buy_qty += 1;
-            } else {
-                $scope.itemsList.push({ item: item, buy_qty: 1 });
-            }
-            $scope.itemToAdd = '';
-            $scope.recomputeAmount();
-        };
-        $scope.removeItem = function(item) {
-            $scope.itemsList.splice($scope.itemsList.indexOf(item), 1);
-            $scope.recomputeAmount();
-        };
-
-        $scope.filterAccounts = function(o) {
-            return o.filter($scope.accountToAdd);
-        };
-
-        $scope.validate = function() {
-            $scope.inRequest = true;
-            _.forEach($scope.itemsList, function(item, i) {
-                item.qty = item.buy_qty * item.item.unit_value;
-            });
-            APIAction.meal({
-                items: $scope.itemsList,
-                accounts: $scope.customersList})
-            .then(function() {
-                init();
-            });
-        };
     }]
 )
 
@@ -92,8 +20,10 @@ angular.module('bars.meal', [
             itemsList: [],
             totalPrice: 0,
             accountToAdd: "",
+            itemToAdd: "",
             account: null,
             inRequest: false,
+            mealName: "",
             init: function() {
                 this.customersList = [ { account: this.account, ratio: 1, amount: 0 } ];
                 this.itemsList = [];
@@ -153,12 +83,13 @@ angular.module('bars.meal', [
                 _.forEach(this.itemsList, function(item, i) {
                     item.qty = item.buy_qty * item.item.unit_value;
                 });
+                var refThis = this;
                 APIAction.meal({
                     items: this.itemsList,
                     accounts: this.customersList
                 })
                 .then(function() {
-                    this.init();
+                    refThis.init();
                 });
             }
         };
