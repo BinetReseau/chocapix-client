04ffbab01909a1e0412135701a29ff4df3902169
Basile Bruneau <basilebruneau@gmail.com>
2014-12-22 09:35:22 +0100

    Create Meal service

--- app/components/meal/meal.js
+++ app/components/meal/meal.js
@@ -4,8 +4,11 @@ angular.module('bars.meal', [
 ])
 
 .controller('meal.ctrl',
-    ['$scope', 'api.models.food', 'api.models.account', 'api.services.action',
-    function($scope, Food, Account, APIAction) {
+    ['$scope', 'api.models.food', 'api.models.account', 'api.services.action', 'bars.meal',
+    function($scope, Food, Account, APIAction, Meal) {
+        Meal.account = $scope.user.account;
+        Meal.init();
+        $scope.meal = Meal;
         function init() {
             $scope.customersList = [ { account: $scope.user.account, ratio: 1, amount: 0 } ];
             $scope.itemsList = [];
@@ -77,6 +80,87 @@ angular.module('bars.meal', [
             .then(function() {
                 init();
             });
+        };
+    }]
+)
+
+.factory('bars.meal',
+    ['api.models.food', 'api.models.account', 'api.services.action',
+    function (Food, Account, APIAction) {
+        return {
+            customersList: [],
+            itemsList: [],
+            totalPrice: 0,
+            accountToAdd: "",
+            account: null,
+            inRequest: false,
+            init: function() {
+                this.customersList = [ { account: this.account, ratio: 1, amount: 0 } ];
+                this.itemsList = [];
+                this.totalPrice = 0;
+                this.accountToAdd = "";
+            },
+            recomputeAmount: function() {
+                var nbParts = 0; // nombre de parts pour le calcul (somme des ratios)
+                _.forEach(this.customersList, function(customer) {
+                    nbParts += customer.ratio;
+                });
+
+                var nbCustomers = this.customersList.length;
+                var nbItems = this.itemsList.length;
+
+                var totalPrice = 0;
+                _.forEach(this.itemsList, function(item, i) {
+                    totalPrice += item.item.price * item.buy_qty * item.item.unit_value;
+                });
+
+                _.forEach(this.customersList, function(customer) {
+                    customer.amount = totalPrice * customer.ratio / nbParts;
+                });
+                this.totalPrice = totalPrice;
+            },
+            addCustomer: function(account, model, label) {
+                var other = _.find(this.customersList, {'account': account});
+                if (!other) {
+                    this.customersList.push({ account: account, ratio: 1, amount: 0 });
+                }
+                this.accountToAdd = '';
+                this.recomputeAmount();
+            },
+            removeCustomer: function(cstmr) {
+                this.customersList.splice(this.customersList.indexOf(cstmr), 1);
+                this.recomputeAmount();
+            },
+            addItem: function(item, model, label) {
+                var other = _.find(this.itemsList, {'item': item});
+                if (other) {
+                    other.buy_qty += 1;
+                } else {
+                    this.itemsList.push({ item: item, buy_qty: 1 });
+                }
+                this.itemToAdd = '';
+                this.recomputeAmount();
+            },
+            removeItem: function(item) {
+                this.itemsList.splice(this.itemsList.indexOf(item), 1);
+                this.recomputeAmount();
+            },
+            filterAccounts: function(o) {
+                return o.filter(this.accountToAdd);
+            },
+            validate: function() {
+                this.inRequest = true;
+                _.forEach(this.itemsList, function(item, i) {
+                    item.qty = item.buy_qty * item.item.unit_value;
+                });
+                APIAction.meal({
+                    items: this.itemsList,
+                    accounts: this.customersList
+                })
+                .then(function() {
+                    this.init();
+                });
+            }
         };
     }]
 )
