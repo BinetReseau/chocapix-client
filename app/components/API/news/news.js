65968fb3be92339f359461a85a612135419fd1ea
tizot <masset.camille@gmail.com>
2015-01-17 20:31:43 +0100

    Add news form in admin panel (not fully operational server-side...)

---
+++ app/components/API/news/news.js
@@ -0,0 +1,26 @@
+'use strict';
+
+angular.module('bars.api.news', [
+    'APIModel'
+    ])
+
+.factory('api.models.news', ['APIModel',
+    function(APIModel) {
+        return new APIModel({
+                url: 'news',
+                type: "News",
+                structure: {
+                    'bar': 'Bar',
+                    'author': 'User'
+                },
+                // methods: {
+                //     'markDeleted': {method:'PUT', url: 'markDeleted'},
+                //     'unMarkDeleted': {method:'PUT', url: 'unMarkDeleted'},
+                //     'filter': function(s) {
+                //         return !this.deleted && (this.name.toLocaleLowerCase().indexOf(s) > -1 ||
+                //             this.keywords.toLocaleLowerCase().indexOf(s) > -1);
+                //     }
+                // }
+            });
+    }])
+;
