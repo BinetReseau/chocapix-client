c56a467b7819acfb86fa7bf19985416b2c31df20
tizot <masset.camille@gmail.com>
2015-02-19 11:34:07 +0100

    Modify bar money threshold in admin panel

--- app/components/admin/admin.js
+++ app/components/admin/admin.js
@@ -115,7 +115,12 @@ angular.module('bars.admin', [
         .state('bar.admin.settings', {
             url: "/settings",
             templateUrl: "components/admin/settings/home.html",
-            controller: 'admin.ctrl.settings'
+            controller: 'admin.ctrl.settings',
+            resolve: {
+                bar: ['api.models.bar' , '$stateParams', function(Bar, $stateParams) {
+                    return Bar.get($stateParams.bar);
+                }]
+            }
         })
         ;
 }])
@@ -391,13 +396,20 @@ function($scope, account_list) {
 )
 // Admin settings
 .controller('admin.ctrl.settings',
-    ['$scope',
-    function($scope) {
+    ['$scope', 'api.models.bar', 'bar', 
+    function($scope, APIBar, bar) {
         $scope.admin.active = 'settings';
         // Seuil d'alerte
-        $scope.moneyLimit = null;
+        $scope.moneyLimit = bar.money_warning_threshold;
         $scope.saveMoneyLimit = function() {
-            // [TODO]
+            if ($scope.moneyLimit >= 0) {
+                bar.money_warning_threshold = $scope.moneyLimit;
+                bar.$save().then(function(b) {
+                    //
+                }, function(errors) {
+                    console.log('Erreur...');
+                });
+            }
         };
         // Agios
         $scope.activateAgio = false;
