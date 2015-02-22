679feb89c10b1182233542262737b758ae798b1a
tizot <masset.camille@gmail.com>
2015-02-22 13:05:43 +0100

    Multiple news with unread-like display on home page

--- app/common/main.js
+++ app/common/main.js
@@ -113,8 +113,32 @@ angular.module('bars.main', [
     ['$scope','news', 'auth.user',
     function($scope, news, AuthUser) {
         $scope.bar.active = 'index';
-        $scope.last_news = function () {
-            return _.sortBy(_.reject(news, 'deleted'), 'last_modified').pop();
+        $scope.list_news = function () {
+            return _.sortBy(_.reject(news, 'deleted'), 'last_modified');
+        };
+
+        function dateDiff(date1, date2){
+            var diff = {}                           // Initialisation du retour
+            var tmp = date2 - date1;
+
+            tmp = Math.floor(tmp/1000);             // Nombre de secondes entre les 2 dates
+            diff.sec = tmp % 60;                    // Extraction du nombre de secondes
+
+            tmp = Math.floor((tmp-diff.sec)/60);    // Nombre de minutes (partie entière)
+            diff.min = tmp % 60;                    // Extraction du nombre de minutes
+
+            tmp = Math.floor((tmp-diff.min)/60);    // Nombre d'heures (entières)
+            diff.hour = tmp % 24;                   // Extraction du nombre d'heures
+
+            tmp = Math.floor((tmp-diff.hour)/24);   // Nombre de jours restants
+            diff.day = tmp;
+
+            return diff.day;
+        }
+        $scope.news_recente = function(news) {
+            var now = new Date();
+            var newsDate = new Date(news.last_modified);
+            return dateDiff(newsDate, now) <= 1;
         };
 
         if (AuthUser.isAuthenticated()) {
