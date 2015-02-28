519750366304298adaaa82363b99c71454e536f8
tizot <masset.camille@gmail.com>
2015-02-28 19:57:15 +0100

    Old stage merge...

---
+++ app/components/root/bar/bar.js
@@ -0,0 +1,42 @@
+'use strict';
+
+angular.module('bars.root.bar', [
+    //
+])
+
+.config(['$stateProvider', '$urlRouterProvider',
+    function($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise("/");
+
+        $stateProvider
+            .state('root.bar', {
+                url: "/bar",
+                abstract: true,
+                template: "<ui-view />"
+            })
+                .state('root.bar.list', {
+                    url: '/list',
+                    templateUrl: "components/root/bar/list.html",
+                    resolve: {
+                        bars_list: ['api.models.bar', function(Bar) {
+                            Bar.reload();
+                            return Bar.all();
+                        }]
+                    },
+                    controller: 'root.ctrl.bar.list'
+                })
+        ;
+    }
+])
+
+.controller('root.ctrl.bar.list',
+    ['$scope', '$stateParams', 'api.models.bar', 'bars_list',
+    function($scope, $stateParams, APIBar, bars_list) {
+        $scope.root.active = 'bars';
+        $scope.bars_list = bars_list;
+        $scope.list_order = 'name';
+        $scope.reverse = false;
+        $scope.searchl = "";
+    }]
+)
+;
