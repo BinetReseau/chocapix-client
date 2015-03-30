93691d8b8e4f27f38a90fe9b4e78a508e44caec4
Basile Bruneau <basilebruneau@gmail.com>
2015-03-30 18:22:22 +0200

    Add User importation in root

--- app/components/root/user/user.js
+++ app/components/root/user/user.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('bars.root.user', [
-    
+
 ])
 .config(['$stateProvider', function($stateProvider) {
     $stateProvider.state('root.user', {
@@ -30,20 +30,47 @@ angular.module('bars.root.user', [
     ;
 }])
 
-.controller('root.ctrl.user.base', 
-    ['$scope', 'user_list', '$state', 
-    function($scope, user_list, $state){
+.controller('root.ctrl.user.base',
+    ['$scope', 'user_list', '$state', 'api.models.user', 'api.models.account', 'APIInterface',
+    function($scope, user_list, $state, User, Account, APIInterface) {
         $scope.root.active = 'user';
         $scope.user_list = user_list;
         $scope.searchl = '';
         $scope.findUser = function(usr) {
             $state.go('root.user.details', {id: usr.id});
         };
+
+        // Importation
+        $scope.lista = "";
+        $scope.oa = [];
+        $scope.$watch('lista', function () {
+            if (JSON.parse($scope.lista)) {
+                $scope.oa = JSON.parse($scope.lista);
+            }
+        });
+        $scope.importUsers = function() {
+            _.forEach($scope.oa, function (ouser) {
+                var nuser = User.create();
+                nuser.firstname = _.capitalize(_.trim(ouser.firstname));
+                nuser.lastname = _.capitalize(_.trim(ouser.lastname));
+                nuser.email = ouser.email;
+                nuser.username = ouser.login;
+                nuser.$save().then(function(u) {
+                    APIInterface.setBar(ouser.section);
+                    var naccount = Account.create();
+                    naccount.owner = u.id;
+                    //naccount.money = ouser.money;
+                    naccount.$save().then(function(a) {
+
+                    });
+                });
+            });
+        };
     }]
 )
 
-.controller('root.ctrl.user.details', 
-    ['$scope', 'api.models.user', 'api.models.role', 'user_req', 
+.controller('root.ctrl.user.details',
+    ['$scope', 'api.models.user', 'api.models.role', 'user_req',
     function($scope, User, Role, user){
         $scope.user = user;
         $scope.userBis = _.clone($scope.user);
