8dd7545f62c878fe3ee02862227e13aa13e41a24
Basile Bruneau <basilebruneau@gmail.com>
2015-08-23 17:10:43 +0200

    Ask for next next appro

--- app/components/admin/food/food.js
+++ app/components/admin/food/food.js
@@ -196,36 +196,52 @@ angular.module('bars.admin.food', [
     ['$scope', 'bar', 'api.models.sellitem',
     function($scope, bar, SellItem) {
         $scope.params = {
+            date_appro_2next: new Date(),
             date_appro_next: new Date(),
             date_appro_1week: new Date(),
             date_appro_2week: new Date()
         };
 
+        function updateDataBuy(params) {
+            bar.sellitem_ranking({date_start: params.date_appro_2week, date_end: params.date_appro_1week});
+            bar.sellitem_ranking({date_start: params.date_appro_1week});
+        }
+
         if (bar.settings.next_scheduled_appro) {
             var next_scheduled_appro = moment(bar.settings.next_scheduled_appro);
             if (moment().isBefore(next_scheduled_appro)) {
+                $scope.params.date_appro_2next = next_scheduled_appro.clone().add(1, 'weeks').toDate();
                 $scope.params.date_appro_next = next_scheduled_appro.toDate();
                 $scope.params.date_appro_1week = next_scheduled_appro.clone().subtract(1, 'weeks').toDate();
                 $scope.params.date_appro_2week = next_scheduled_appro.clone().subtract(2, 'weeks').toDate();
             } else {
+                $scope.params.date_appro_2next = next_scheduled_appro.clone().add(2, 'weeks').toDate();
                 $scope.params.date_appro_next = next_scheduled_appro.clone().add(1, 'weeks').toDate();
                 $scope.params.date_appro_1week = next_scheduled_appro.toDate();
                 $scope.params.date_appro_2week = next_scheduled_appro.clone().subtract(1, 'weeks').toDate();
             }
         }
 
+        updateDataBuy($scope.params);
+
+        $scope.dateChange = updateDataBuy;
+
         // Utils functions for datepicker
+        $scope.date_appro_2next_opened = false;
         $scope.date_appro_next_opened = false;
         $scope.date_appro_1week_opened = false;
         $scope.date_appro_2week_opened = false;
         $scope.open = function($event, w) {
             $event.preventDefault();
             $event.stopPropagation();
+            $scope.date_appro_2next_opened = false;
             $scope.date_appro_next_opened = false;
             $scope.date_appro_1week_opened = false;
             $scope.date_appro_2week_opened = false;
             if (w == 'date_appro_next') {
                 $scope.date_appro_next_opened = true;
+            } else if (w == 'date_appro_2next') {
+                $scope.date_appro_2next_opened = true;
             } else if (w == 'date_appro_1week') {
                 $scope.date_appro_1week_opened = true;
             } else if (w == 'date_appro_2week') {
