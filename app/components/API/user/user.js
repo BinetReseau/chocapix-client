0f00578d9035e7a59a4968cd7bcfab1170609951
wilhelmbh <wilhelmhb@live.fr>
2015-09-30 11:19:11 +0200

    rectifications dues aux remarques, refresh en toujours en place

--- app/components/API/user/user.js
+++ app/components/API/user/user.js
@@ -9,9 +9,6 @@ angular.module('bars.api.user', [
         var model = new APIModel({
                 url: 'user',
                 type: "User",
-                /*structure: {
-                	suggested_items: "SuggestedItem"
-                },*/
                 methods: {
                     'me': {url: 'me', static: true},
                 }
