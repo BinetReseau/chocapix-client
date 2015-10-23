6ffb054d59f3a9c36d15bd069c70dca38a79e899
wilhelmhb <wilhelmhb@live.fr>
2015-10-23 17:47:37 +0200

    Update food.js

--- app/components/admin/food/food.js
+++ app/components/admin/food/food.js
@@ -50,29 +50,6 @@ angular.module('bars.admin.food', [
                 }]
             }
         })
-        .state('bar.admin.food.suggested_items_list', {
-        	abstract: true,
-        	url: "/suggested-item",
-        	controller: 'admin.ctrl.food.suggested_items_list',
-        	template: '<ui-view />'
-    	})
-        .state('bar.admin.food.suggested_items_list.list', {
-            url: "/list",
-            templateUrl: "components/admin/food/suggested-items-list.html",
-            controller: 'admin.ctrl.food.suggested_items_list.list',
-            resolve: {
-                suggested_items: ['api.models.suggested_items', function(suggested_items) {
-                    suggested_items.clear();
-                    suggested_items.reload();
-                    return suggested_items.all();
-                }]
-            }
-        })
-        .state('bar.admin.food.suggested_items_list.edit', {//allow administrator to edit suggestions
-            url: '/edit/:id',
-            templateUrl: "components/admin/food/suggested-item-form.html",
-            controller: 'admin.ctrl.food.suggested_items_list.edit'
-        })
         .state('bar.admin.food.graphs', {
             url: "/graphs",
             templateUrl: "components/admin/food/graphs.html",
@@ -991,46 +968,6 @@ angular.module('bars.admin.food', [
         };
     }
 ])
-.controller('admin.ctrl.food.suggested_items_list',
-    ['$scope', function ($scope) {
-        $scope.admin.active = 'suggested_item';
-    }]
-)
-.controller('admin.ctrl.food.suggested_items_list.list',
-    ['$scope', 'api.models.suggested_items', 'api.models.user', 'suggested_items',
-    function($scope, SuggestedItem, User, suggested_items) {
-        $scope.admin.active = 'suggested_item';
-        $scope.suggested_items = suggested_items;
-        $scope.list_order = 'name';
-        $scope.trash = function(suggested_item) {
-            suggested_item.already_added = true;
-            suggested_item.$save();
-        };
-        $scope.untrash = function(suggested_item) {
-            suggested_item.already_added = false;
-            suggested_item.$save();
-        };
-        $scope.suggesteditem_delete = function(suggested_item) {
-            suggested_item.$delete();
-        };
-    }
-])
-.controller('admin.ctrl.food.suggested_items_list.edit',
-    ['$scope', 'api.models.suggested_items', 'api.models.user', '$stateParams', '$state',
-    function($scope, SuggestedItem, User, $stateParams, $state) {
-        $scope.formType = 'edit';
-        $scope.admin.active = 'suggested_item';
-        $scope.suggested_item = SuggestedItem.get($stateParams.id);
-        $scope.saveSuggestedItem = function() {
-            $scope.suggested_item.name = $scope.suggested_item.name == '' ? 'Informations' : $scope.suggested_item.name;
-            $scope.suggested_item.$save().then(function(newSuggestedItem) {
-                $state.go('bar.admin.food.suggested_items_list.list');
-            }, function(errors) {
-                    // TODO: display form errors
-            });
-        };
-    }]
-)
 .factory('admin.appro',
     ['api.models.stockitem', 'api.services.action',
     function (StockItem, APIAction) {
