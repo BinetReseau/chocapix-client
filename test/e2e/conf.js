df02712ef2219a39b7cf4f5a3f9f65b980b024b4
Basile Bruneau <basilebruneau@gmail.com>
2015-11-14 19:36:02 +0100

    First test is working \o/

--- test/e2e/conf.js
+++ test/e2e/conf.js
@@ -3,5 +3,17 @@ exports.config = {
     specs: ['specs/*.js'],
     capabilities: {
         'browserName': 'chrome'
+    },
+    onPrepare: function() {
+        /* global angular: false, browser: false, jasmine: false */
+
+        // Disable animations so e2e tests run more quickly
+        var disableNgAnimate = function() {
+          angular.module('disableNgAnimate', []).run(['$animate', function($animate) {
+            $animate.enabled(false);
+          }]);
+        };
+
+        browser.addMockModule('disableNgAnimate', disableNgAnimate);
     }
 };
