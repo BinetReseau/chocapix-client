25d1e3c621176a8f28ea65c2642d5818b8ed0ed2
wilhelmbh <wilhelmhb@live.fr>
2015-11-26 16:55:48 +0100

    allow any customer to vote for himself only, and to take back his vote

--- app/components/API/suggested_items/suggested_items.js
+++ app/components/API/suggested_items/suggested_items.js
@@ -20,13 +20,17 @@ angular.module('bars.api.suggested_items', [
                 votedByCurrentUser: function() {
                     return AuthUser.user && this.votedBy(AuthUser.user);
                 },
-                addVoteBy: function(user) {
-                    this.voters_list.push(user.id);
-                    return this.$save();
+                vote: function() {
+                    return APIInterface.request({
+                        'url': 'suggested_items/' + this.id + '/vote',
+                        'method': 'POST'})
                 },
-                addVoteByCurrentUser: function() {
-                    return this.addVoteBy(AuthUser.user);
-                }
+                unvote: function() {
+                    return APIInterface.request({
+                        'url': 'suggested_items/' + this.id + '/unvote',
+                        'method': 'POST',
+                        'data': {}});
+                },
             }
         });
         model.request = function(params) {
@@ -42,7 +46,22 @@ angular.module('bars.api.suggested_items', [
     return {
         restrict: 'E',
         scope: {
-            suggesteditem: '=suggesteditem' //l'objet suggesteditem, définit dans l'élément parent
+            suggesteditem: '=suggesteditem', //l'objet suggesteditem, définit dans l'élément parent
+        },
+        link: function($scope) {
+            $scope.vote = function(suggesteditem) {
+                console.log('coucou');
+                suggesteditem.vote().then(function(suggesteditem) {
+                    suggesteditem.$reload();
+                });
+            };
+        
+            $scope.unvote = function(suggesteditem) {
+                console.log('ok');
+                suggesteditem.unvote().then(function(suggesteditem) {
+                    suggesteditem.$reload();
+                });
+            };
         },
         templateUrl: 'components/API/suggested_items/directive.html' //injecté dans le code HTML de cette page
     };
