6e34fd123ed5dba089a982e74e235f8ae44dbe64
tizot <masset.camille@gmail.com>
2016-01-18 00:14:27 +0100

    Add form to edit a Suggested Item

--- app/components/admin/suggesteditem/suggesteditem.js
+++ app/components/admin/suggesteditem/suggesteditem.js
@@ -50,35 +50,31 @@ angular.module('bars.admin.suggesteditem', [
     }
 ])
 
-// .controller('admin.ctrl.suggesteditem.edit',
-//     ['$scope', '$timeout', 'api.models.suggested_items', 'api.models.user', '$stateParams', '$state',
-//     function($scope, $timeout, SuggestedItem, User, $stateParams, $state) {
-//         $scope.admin.active = 'suggested_item';
-//         $scope.suggestion = SuggestedItem.get($stateParams.id);
-//         $scope.suggestedItems = SuggestedItem.all();
-//         var suggested_item = $scope.suggestion;
-//
-//         $scope.saveSuggestedItem = function(suggestion) { //add a suggestion to the list
-//             if (!suggestion) {
-//                 return;
-//             }
-//             suggested_item.name = suggestion;
-//             //verify that the new suggestion doesn't already exist
-//             if (_.find($scope.suggestedItems, function (item) {
-//                 console.log(item);
-//                 console.log(suggested_item);
-//                 return (_.deburr(item.name.toLocaleLowerCase()) === _.deburr(suggested_item.name.toLocaleLowerCase()))&&(item.id !== suggested_item.id);
-//             })) {
-//                 $scope.suggestion.alreadyAdded = true;
-//                 console.log('existe déjà');
-//                 $timeout(function () {
-//                     $scope.suggestion.alreadyAdded = false;
-//                 }, 2500);
-//             } else {
-//                 suggested_item.$save();
-//                 $state.go('bar.admin.food.suggested_items_list.list');
-//             }
-//         };
-//     }]
-// )
+.controller('admin.ctrl.suggesteditem.edit',
+    ['$scope', '$timeout', 'api.models.suggesteditem', 'api.models.user', '$stateParams', '$state', 'suggesteditems',
+    function($scope, $timeout, SuggestedItem, User, $stateParams, $state, suggesteditems) {
+        $scope.suggesteditem = SuggestedItem.get($stateParams.id);
+        $scope.alreadyAdded = false;
+
+        $scope.saveSuggestedItem = function(suggesteditem) { //add a suggestion to the list
+            if (!suggesteditem.name) {
+                return;
+            }
+
+            // Verify that the new suggestion doesn't already exist
+            if (_.find(suggesteditems, function (item) {
+                return (_.deburr(item.name.toLocaleLowerCase()) === _.deburr(suggesteditem.name.toLocaleLowerCase())) && (item.id !== suggesteditem.id);
+            })) {
+                $scope.alreadyAdded = true;
+                $timeout(function () {
+                    $scope.alreadyAdded = false;
+                }, 2500);
+            } else {
+                suggesteditem.$save().then(function() {
+                    $state.go('bar.admin.suggesteditem.list');
+                });
+            }
+        };
+    }]
+)
 ;
