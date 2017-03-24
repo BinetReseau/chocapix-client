77595e350570f431323c09534720dfb98f11952a
Florian Denis <contact@floriandenis.net>
2017-03-24 17:03:46 +0100

    grunt serve : listen on the good interfaces

--- app/config.js
+++ app/config.js
@@ -1,4 +1,4 @@
-var API_URL = "http://127.0.0.1:8000";
+var API_URL = "http://129.104.201.201:8000";
 var OFF_URL = "http://fr.openfoodfacts.org/api/v0/produit";
 //var API_URL = "https://api.chocapix.binets.fr";
 //var OFF_URL = "https://off.chocapix.binets.fr";
